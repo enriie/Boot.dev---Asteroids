fd49fca8cea16e01a2a3a5d739f3e99a7085cf87
enriie <j.fx.jankovic@gmail.com>
2024-10-24 21:43:22 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from constants import *
 import pygame
 
 def main():
     print("Starting asteroids!")
+    print("Screen width: %d\nScreen height: %d" % (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Boot.dev / Asteroids")
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
